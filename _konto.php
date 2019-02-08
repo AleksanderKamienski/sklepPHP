06ea75312de646bab17cdcc99bd108107ab08673
Alek <aleksnaderkamienski.97@wp.pl>
2019-02-08 13:42:04 +0100

    koszyk fix2

--- _konto.php
+++ _konto.php
@@ -22,11 +22,7 @@ if(!isset($_SESSION['zalogowany']))
 	header('Location: https://demo-php-store.herokuapp.com/_sklepInternetowy.php');
 	exit();
 }
-if(isset($_SESSION['admin']))
-{
-	header('Location: https://demo-php-store.herokuapp.com/_kontoAdmin.php');
-	exit();
-}
+
 
 ?>
    
