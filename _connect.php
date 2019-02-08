7b70fcc5459f569abef4608e578defcc0cacce3a
Alek <aleksnaderkamienski.97@wp.pl>
2019-02-08 14:03:50 +0100

    fixx

--- _connect.php
+++ _connect.php
@@ -4,10 +4,11 @@
 	$db_user = "g08c8yb6qu200eyh";
 	$db_password = "imkaai9dgkqwgqrx";
 	$db_name = "m4z6hoy12c0q1t3f";
-
-	if(!isset($_SESSION['zalogowany']))
+/*
+	if(!isset($_SESSION['zalogowany']==true))
 	{
 		header('Location: https://demo-php-store.herokuapp.com/_sklepInternetowy.php');
 		exit();
 	}
+	*/
 ?>
